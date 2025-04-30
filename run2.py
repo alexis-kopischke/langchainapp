140c7d798e718728bc651005765c34281c7df077
alexis-kopischke <149273039+alexis-kopischke@users.noreply.github.com>
2025-04-29 21:49:43 -0500

    run2.py

--- run2.py
+++ run2.py
@@ -6,6 +6,7 @@ from langchain.llms import OpenAI
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import Chroma
+from langchain.vectorstores import FAISS
 from langchain.chains import RetrievalQA
 
 def generate_response(uploaded_file, openai_api_key, query_text):
